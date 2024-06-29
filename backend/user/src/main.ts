cb238340f15352af875e30751e2d80fc0bb75993
haonguyen <thienhamaimai@gmail.com>
2024-06-29 17:47:20 +0700

    update src

---
+++ backend/user/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+        requestTimeout: 3000,
+      }
+    }
+  });
+  await app.listen();
+}
+bootstrap();
